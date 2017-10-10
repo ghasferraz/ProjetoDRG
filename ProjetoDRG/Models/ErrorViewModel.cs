e51291ce9fd3fc658a00719a4b9199466fe4addb
Matheus Fontanetti <matheusfontanetti@hotmail.com>
2017-10-10 13:25:51 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoDRG/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjetoDRG.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
